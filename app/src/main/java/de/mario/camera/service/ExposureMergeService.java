5dbe1ef91b0391282f616e845ef27262acf62451
Mario-S <spindizzy@outlook.com>
2015-07-29 16:14:28 +0200

    extended service to merge picture with different exposure times

--- app/src/main/java/de/mario/camera/service/ExposureMergeService.java
+++ app/src/main/java/de/mario/camera/service/ExposureMergeService.java
@@ -4,7 +4,19 @@ import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.photo.MergeMertens;
+import org.opencv.photo.Photo;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -14,7 +26,7 @@ import java.util.Map;
  */
 public class ExposureMergeService extends IntentService {
 
-    private static final String PARAM_PICS = "de.mario.camera.extra.PICS";
+    static final String PARAM_PICS = "de.mario.camera.extra.PICS";
 
 
     /**
@@ -46,7 +58,40 @@ public class ExposureMergeService extends IntentService {
      */
     private void handleProcessing(String [] pictures) {
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        List<Mat> images = loadImages(pictures);
+
+        Mat fusion = new Mat();
+        MergeMertens mergeMertens = Photo.createMergeMertens();
+        mergeMertens.process(images, fusion);
+
+        File out = new File(createFileName(pictures[0]));
+        write(fusion, out);
+
     }
 
+    private String createFileName(String src) {
+        int pos = src.lastIndexOf(".") ;
+        String preffix = src.substring(0, pos - 1);
+        String suffix = src.substring(pos);
+        return preffix + "merged" + suffix;
+    }
+
+    private void write(Mat fusion, File out) {
+        Mat result = new Mat();
+        Scalar scalar = new Scalar(255, 255, 255);
+        Core.multiply(fusion, scalar, result);
+        Imgcodecs.imwrite(out.getPath(), result);
+    }
+
+    private List<Mat> loadImages(String[] pics) {
+        List<Mat> imgs = new ArrayList<>();
+
+        for(String pic : pics){
+            File f = new File(pic);
+            Mat read = Imgcodecs.imread(f.getPath());
+            imgs.add(read);
+        }
+
+        return imgs;
+    }
 }
