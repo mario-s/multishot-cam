daa534119b1497df2e047914068498964df80e01
Mario-S <spindizzy@outlook.com>
2016-04-04 20:49:06 +0200

    added method to determine preview & pic size

--- app/src/main/java/de/mario/camera/Preview.java
+++ app/src/main/java/de/mario/camera/Preview.java
@@ -2,80 +2,204 @@ package de.mario.camera;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.hardware.Camera;
 import android.util.Log;
 import android.view.Display;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.RelativeLayout;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 
-	private SurfaceHolder mHolder;
-	private Camera camera;
+    private SurfaceHolder mHolder;
+    private Camera camera;
+    private List<Camera.Size> previewSizeList;
+    private List<Camera.Size> pictureSizeList;
+    protected Camera.Size previewSize;
+    protected Camera.Size pictureSize;
+    private boolean surfaceConfiguring = false;
+    private int centerPosX = -1;
+    private int centerPosY;
 
-	Preview(Context context, Camera camera) {
-		super(context);
-		this.camera = camera;
+    Preview(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
 
 
-		// Install a SurfaceHolder.Callback so we get notified when the
-		// underlying surface is created and destroyed.
-		mHolder = getHolder();
-		mHolder.addCallback(this);
-		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-	}
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-		// The Surface has been created, acquire the camera and tell it where
-		// to draw.
-		try {
-			camera.setPreviewDisplay(holder);
-			camera.startPreview();
-		} catch (IOException e) {
-			Log.e(PhotoActivable.DEBUG_TAG, e.getMessage(), e);
-		}
-	}
+        Camera.Parameters cameraParams = this.camera.getParameters();
+        previewSizeList = cameraParams.getSupportedPreviewSizes();
+        pictureSizeList = cameraParams.getSupportedPictureSizes();
+    }
 
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		// Surface will be destroyed when we return, so stop the preview.
-		// Because the CameraDevice object is not a shared resource, it's very
-		// important to release it when the activity is paused.
-	}
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, acquire the camera and tell it where
+        // to draw.
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (IOException e) {
+            Log.e(PhotoActivable.DEBUG_TAG, e.getMessage(), e);
+        }
+    }
 
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        doSurfaceChanged(w, h);
+    }
 
-		if (mHolder.getSurface() != null) {
-			camera.stopPreview();
-			surfaceCreated(mHolder);
-			int angle = getAngle();
-			camera.setDisplayOrientation(angle);
-		}
-	}
+    private void doSurfaceChanged(int width, int height) {
+        camera.stopPreview();
 
-	private int getAngle() {
-		int angle;
-		Display display = ((Activity)getContext()).getWindowManager().getDefaultDisplay();
-		switch (display.getRotation()) {
-			case Surface.ROTATION_90:
-				angle = 0;
-				break;
-			case Surface.ROTATION_180:
-				angle = 270;
-				break;
-			case Surface.ROTATION_270:
-				angle = 180;
-				break;
-			default:
-				angle = 90;
-				break;
-		}
-		return angle;
-	}
+        boolean portrait = isPortrait();
+        if(!surfaceConfiguring) {
+            previewSize = determinePreviewSize(portrait, width, height);
+            pictureSize = determinePictureSize(previewSize);
+
+            surfaceConfiguring = adjustSurfaceLayoutSize(previewSize, portrait, width, height);
+        }
+
+
+        int angle = getAngle();
+        camera.setDisplayOrientation(angle);
+
+        camera.startPreview();
+    }
+
+    private int getAngle() {
+        int angle;
+        Display display = ((Activity) getContext()).getWindowManager().getDefaultDisplay();
+        switch (display.getRotation()) {
+            case Surface.ROTATION_90:
+                angle = 0;
+                break;
+            case Surface.ROTATION_180:
+                angle = 270;
+                break;
+            case Surface.ROTATION_270:
+                angle = 180;
+                break;
+            default:
+                angle = 90;
+                break;
+        }
+        return angle;
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+
+    private Camera.Size determinePreviewSize(boolean portrait, int reqWidth, int reqHeight) {
+        // Meaning of width and height is switched for preview when portrait,
+        // while it is the same as user's view for surface and metrics.
+        // That is, width must always be larger than height for setPreviewSize.
+        int reqPreviewWidth; // requested width in terms of camera hardware
+        int reqPreviewHeight; // requested height in terms of camera hardware
+        if (portrait) {
+            reqPreviewWidth = reqHeight;
+            reqPreviewHeight = reqWidth;
+        } else {
+            reqPreviewWidth = reqWidth;
+            reqPreviewHeight = reqHeight;
+        }
+
+        // Adjust surface size with the closest aspect-ratio
+        float reqRatio = ((float) reqPreviewWidth) / reqPreviewHeight;
+        float curRatio, deltaRatio;
+        float deltaRatioMin = Float.MAX_VALUE;
+        Camera.Size retSize = null;
+        for (Camera.Size size : previewSizeList) {
+            curRatio = ((float) size.width) / size.height;
+            deltaRatio = Math.abs(reqRatio - curRatio);
+            if (deltaRatio < deltaRatioMin) {
+                deltaRatioMin = deltaRatio;
+                retSize = size;
+            }
+        }
+
+        return retSize;
+    }
+
+    private Camera.Size determinePictureSize(Camera.Size previewSize) {
+        Camera.Size retSize = null;
+        for (Camera.Size size : pictureSizeList) {
+            if (size.equals(previewSize)) {
+                return size;
+            }
+        }
+
+        // if the preview size is not supported as a picture size
+        float reqRatio = ((float) previewSize.width) / previewSize.height;
+        float curRatio, deltaRatio;
+        float deltaRatioMin = Float.MAX_VALUE;
+        for (Camera.Size size : pictureSizeList) {
+            curRatio = ((float) size.width) / size.height;
+            deltaRatio = Math.abs(reqRatio - curRatio);
+            if (deltaRatio < deltaRatioMin) {
+                deltaRatioMin = deltaRatio;
+                retSize = size;
+            }
+        }
+
+        return retSize;
+    }
+
+    private boolean adjustSurfaceLayoutSize(Camera.Size previewSize, boolean portrait,
+                                            int availableWidth, int availableHeight) {
+        float tmpLayoutHeight, tmpLayoutWidth;
+        if (portrait) {
+            tmpLayoutHeight = previewSize.width;
+            tmpLayoutWidth = previewSize.height;
+        } else {
+            tmpLayoutHeight = previewSize.height;
+            tmpLayoutWidth = previewSize.width;
+        }
+
+        float factH, factW, fact;
+        factH = availableHeight / tmpLayoutHeight;
+        factW = availableWidth / tmpLayoutWidth;
+        if (factH < factW) {
+            fact = factH;
+        } else {
+            fact = factW;
+        }
+
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) getLayoutParams();
+
+        int layoutHeight = (int) (tmpLayoutHeight * fact);
+        int layoutWidth = (int) (tmpLayoutWidth * fact);
+
+        boolean layoutChanged;
+        if ((layoutWidth != this.getWidth()) || (layoutHeight != this.getHeight())) {
+            layoutParams.height = layoutHeight;
+            layoutParams.width = layoutWidth;
+            if (centerPosX >= 0) {
+                layoutParams.topMargin = centerPosY - (layoutHeight / 2);
+                layoutParams.leftMargin = centerPosX - (layoutWidth / 2);
+            }
+            this.setLayoutParams(layoutParams); // this will trigger another surfaceChanged invocation.
+            layoutChanged = true;
+        } else {
+            layoutChanged = false;
+        }
+
+        return layoutChanged;
+    }
+
+    private boolean isPortrait() {
+        return getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+    }
 
 }
