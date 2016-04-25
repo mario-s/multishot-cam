cc4e7c7be73368e70fd7def21da90989ee985c23
Mario-S <spindizzy@outlook.com>
2016-04-25 14:57:54 +0200

    extended start up dialog

--- app/src/main/java/de/mario/camera/StartupDialog.java
+++ app/src/main/java/de/mario/camera/StartupDialog.java
@@ -4,6 +4,10 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+
 /**
  * @author Mario
  */
@@ -16,14 +20,52 @@ class StartupDialog {
     }
 
     void show() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle(R.string.app_name).setMessage(context.getString(R.string.startup_text));
-        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+        AlertDialog.Builder builder = newBuilder();
+        builder.setMessage(context.getString(R.string.startup_text));
+        builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, context, new Callback());
+            }
+        });
+        builder.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();
             }
         });
         builder.show();
+    }
+
+    private AlertDialog.Builder newBuilder() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(R.string.app_name);
+        return builder;
+    }
+
+    private class Callback extends BaseLoaderCallback {
+
+        Callback(){
+            super(context);
+        }
+
+        @Override
+        public void onManagerConnected(int status) {
+            super.onManagerConnected(status);
+            //show a message that we found a manager
+            if(status == LoaderCallbackInterface.SUCCESS){
+
+                AlertDialog.Builder builder = newBuilder();
+                builder.setMessage(context.getString(R.string.open_cv_present));
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
+            }
+        }
     }
 }
