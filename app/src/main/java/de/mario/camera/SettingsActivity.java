327dc9a3374276301eacee99f68b096a78cf79d5
Mario-S <spindizzy@outlook.com>
2016-05-07 19:42:21 +0200

    basic for image resolution

--- app/src/main/java/de/mario/camera/SettingsActivity.java
+++ app/src/main/java/de/mario/camera/SettingsActivity.java
@@ -1,7 +1,9 @@
 package de.mario.camera;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -22,7 +24,26 @@ public class SettingsActivity extends PreferenceActivity {
         super.onPostCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences);
+        addImageResolutions();
     }
 
+    private void addImageResolutions() {
+        // Get the Preference Category which we want to add the ListPreference to
+        PreferenceCategory targetCategory = (PreferenceCategory) findPreference("camera");
+        ListPreference customListPref = new ListPreference(this);
+
+        CharSequence[] entries = new CharSequence[]{"One", "Two", "Three"};
+        CharSequence[] entryValues = new CharSequence[]{ "1", "2", "3" };
+
+        // IMPORTANT - This is where set entries...looks OK to me
+        customListPref.setEntries(entries);
+        customListPref.setEntryValues(entryValues);
+        customListPref.setTitle("Title");
+        customListPref.setSummary("This is the summary");
+        customListPref.setPersistent(true);
+
+        // Add the ListPref to the Pref category
+        targetCategory.addPreference(customListPref);
+    }
 
 }
--- app/src/main/java/de/mario/camera/SettingsValue.java
+++ app/src/main/java/de/mario/camera/SettingsValue.java
@@ -4,7 +4,8 @@ package de.mario.camera;
  */
 public enum SettingsValue {
     GEO_TAGGING("geotagging"), SHUTTER_SOUND("shutterSound"), PROCESS_HDR("processHdr"),
-    SHUTTER_DELAY("shutterDelayTime"), GRID("grid"), TRACE("trace");
+    SHUTTER_DELAY("shutterDelayTime"), GRID("grid"), TRACE("trace") ,
+    IMAGE_RESOLUTION("imageResolution");
 
     private String value;
 
