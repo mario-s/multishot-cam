6c4d21078afb785caca8cdc6908545b7644810fc
Mario-S <spindizzy@outlook.com>
2016-06-19 12:07:11 +0200

    extended test

--- app/src/test/java/de/mario/camera/controller/ExposureValuesFactoryTest.java
+++ app/src/test/java/de/mario/camera/controller/ExposureValuesFactoryTest.java
@@ -11,6 +11,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 /**
  */
@@ -25,10 +26,19 @@ public class ExposureValuesFactoryTest {
     @Before
     public void setUp() {
         classUnderTest = new ExposureValuesFactory(parameters);
+        given(parameters.getMinExposureCompensation()).willReturn(-4);
+        given(parameters.getMaxExposureCompensation()).willReturn(4);
     }
 
     @Test
     public void testSequence_One() {
+        List<Integer> vals = classUnderTest.getValues(1);
+
+        assertEquals(5, vals.size());
+    }
+
+    @Test
+    public void testSequence_Default() {
         List<Integer> vals = classUnderTest.getValues(0);
 
         assertEquals(3, vals.size());
