80a354d9def12389ef1e287a525be95fd62e4121
Mario-S <spindizzy@outlook.com>
2016-07-10 22:10:14 +0200

    moved preview package

---
+++ app/src/test/java/de/mario/photo/exif/ExifWriterTest.java
@@ -0,0 +1,51 @@
+package de.mario.photo.exif;
+
+import android.media.ExifInterface;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ExifWriterTest {
+    @Mock
+    private ExifInterface exifInterface;
+
+    private File src;
+
+    private Map<ExifTag, String> map;
+
+    private ExifWriter classUnderTest;
+
+    @Before
+    public void setUp() {
+        src = new File(".");
+        map = new HashMap<>();
+        map.put(ExifTag.MAKE, "test");
+
+        classUnderTest = new ExifWriter(){
+            @Override
+            ExifInterface getExifInterface(File file) throws IOException {
+                return exifInterface;
+            }
+        };
+    }
+
+    @Test
+    public void testAddTags() throws IOException {
+
+        classUnderTest.addTags(src, map);
+        verify(exifInterface).saveAttributes();
+    }
+}
