8a28c02a7791fbca30b14941fcd3bd81e499de3b
Mario-S <spindizzy@outlook.com>
2016-07-19 21:53:21 +0200

    added test

---
+++ app/src/test/java/de/mario/photo/settings/SettingsAccessITest.java
@@ -0,0 +1,46 @@
+package de.mario.photo.settings;
+
+import android.os.Bundle;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import de.mario.photo.BuildConfig;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.internal.util.reflection.Whitebox.setInternalState;
+
+/**
+ * Integration Test for {@link SettingsActivity} and {@link SettingsAccess}.
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 18)
+public class SettingsAccessITest {
+
+    private SettingsAccess classUnderTest;
+
+    @Before
+    public void setUp() {
+        SettingsActivity activity = Robolectric.buildActivity(SettingsActivity.class).create().get();
+        Bundle bundle = new Bundle();
+        activity.onPostCreate(bundle);
+
+        classUnderTest = new SettingsAccess();
+        setInternalState(classUnderTest, "context", activity.getBaseContext());
+    }
+
+    @Test
+    public void testDelay() {
+        assertThat(classUnderTest.getDelay(), is(0));
+    }
+
+    @Test
+    public void testIsGeoTagEnabled() {
+        assertThat(classUnderTest.isGeoTaggingEnabled(), is(false));
+    }
+}
