f1733f0aa704b731b2bf6e4a77ea442594ad2c06
Mario-S <spindizzy@outlook.com>
2017-04-23 14:34:20 +0200

    updated test

--- app/src/test/java/de/mario/photo/view/OrientationNoiseFilterTest.java
+++ app/src/test/java/de/mario/photo/view/OrientationNoiseFilterTest.java
@@ -3,9 +3,15 @@ package de.mario.photo.view;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
 public class OrientationNoiseFilterTest {
+
+    private static final BigDecimal ERROR = new BigDecimal(1);
 
     private OrientationNoiseFilter classUnderTest;
 
@@ -26,13 +32,13 @@ public class OrientationNoiseFilterTest {
     public void testFilter_vertical_toLeft() {
         classUnderTest.filter(6);
         int result = classUnderTest.filter(358);
-        assertEquals(3, result);
+        assertThat(new BigDecimal(result), is(closeTo(new BigDecimal(3), ERROR)));
     }
 
     @Test
     public void testFilter_vertical_toRight() {
         classUnderTest.filter(358);
         int result = classUnderTest.filter(6);
-        assertEquals(0, result);
+        assertThat(new BigDecimal(result), is(closeTo(new BigDecimal(360), ERROR)));
     }
 }
